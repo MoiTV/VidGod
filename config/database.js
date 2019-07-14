ea2988b60d4c692cac20b03c993bc481b938dc91
Moises Valdez <46477259+MoiTV@users.noreply.github.com>
2019-07-14 08:31:02 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb+srv://moises559:xbox360559!@cluster1-lg2g9.mongodb.net/test?retryWrites=true&w=majority'
+        mongoURI: ''
     }
 } else {
     module.exports = {
-        mongoURI: 'mongodb://localhost/vidjot-dev'
+        mongoURI: ''
     }
 }
